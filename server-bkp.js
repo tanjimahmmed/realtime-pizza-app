3a2ea590300845bdf1193de7994f9f05abc28887
tanjimahmmed <tanjimahmmed.8@gmail.com>
2021-03-31 15:30:57 +0600

    add to cart

--- server-bkp.js
+++ server-bkp.js
@@ -10,55 +10,47 @@ const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
 
-// Database connection
+// Database connection 
 const url = 'mongodb://localhost/pizza';
 mongoose.connect(url, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('Database connected...');
 }).catch(err => {
-    console.log('Connection failed...')
+    console.log('connection failed...');
 });
 
 
 // Session store
-
 let mongoStore = new MongoDbStore({
-                mongooseConnection: connection,
-                collection: 'sessions'
-            })
+                    mongooseConnection: connection,
+                    collection: 'sessions'
+                })
 
 
-// Session config
+// session config
 app.use(session({
     secret: process.env.COOKIE_SECRET,
-    resave: false,
+    resave: false, 
     store: mongoStore,
     saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
+    cookie: {maxAge: 1000 * 60 * 60 * 24} 
 }))
 
+app.use(flash()) 
 
-app.use(flash())
 // Assets
 app.use(express.static('public'))
-app.use(express.json())
 
-// Global middleware
-app.use((req, res, next) => {
-    res.locals.session = req.session
-    next()
-})
 
-// set Template engine
+// set template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 require('./routes/web')(app)
 
-// const server = 
-app.listen(PORT , () => {
-            console.log(`Listening on port ${PORT}`)
-        })
 
+app.listen(PORT, () => {
+    console.log(`Listen to port xyz tj ${PORT}`);
+})
